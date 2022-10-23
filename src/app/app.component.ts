9a0488ef7b0930ec2be675bd228b95c46f41e22e
EugeneMNS <evgen.str4@gmail.com>
2022-10-23 19:36:38 +0300

    feat: directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+
+interface Fruit {
+  id: string
+  name: string
+  price: number
+}
 
 @Component({
-  selector: 'app-root',
+  selector: 'inst-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'instagram';
+  fruits: Fruit[] = [
+    { id: '1', name: 'Apple', price: 1.99 },
+    { id: '2', name: 'Orange', price: 2.99 },
+    { id: '3', name: 'Banana', price: 123.99 },
+    { id: '4', name: 'Grape', price: 4.99 },
+    { id: '5', name: 'Pineapple', price: 5.99 },
+    { id: '6', name: 'Strawberry', price: 6.99 },
+    { id: '7', name: 'Watermelon', price: 7.99 },
+    { id: '8', name: 'Mango', price: 18.99 },
+    { id: '9', name: 'Peach', price: 9.99 },
+  ]
 }
