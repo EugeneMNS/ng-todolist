54380c480b53479769e1828fd5ee2a16d9d001e1
EugeneMNS <evgen.str4@gmail.com>
2022-10-24 15:26:06 +0300

    unsubscribe

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,39 +1,58 @@
-import { Component, OnInit } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
 import { BaseResponse, Todo, TodosService } from '../../services/todos.service'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'inst-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss'],
 })
-export class TodosComponent implements OnInit {
+export class TodosComponent implements OnInit, OnDestroy {
   todos: Todo[] = []
+  error = ''
+  subscriptions: Subscription = new Subscription()
 
   constructor(private todosService: TodosService) {}
 
   ngOnInit(): void {
     this.getTodos()
   }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe()
+  }
+
   getTodos() {
-    this.todosService.getTodos().subscribe(res => {
-      this.todos = res
-    })
+    this.subscriptions.add(
+      this.todosService.getTodos().subscribe({
+        next: res => {
+          this.todos = res
+        },
+        error: (error: HttpErrorResponse) => {
+          this.error = error.message
+        },
+      })
+    )
   }
 
   createTodo() {
     const randomNumber = Math.floor(Math.random() * 100)
     const title = 'Angular ' + randomNumber
-    this.todosService.createTodo(title).subscribe(res => {
-      const newTodo = res.data.item
-      this.todos.unshift(newTodo)
-    })
+    this.subscriptions.add(
+      this.todosService.createTodo(title).subscribe(res => {
+        const newTodo = res.data.item
+        this.todos.unshift(newTodo)
+      })
+    )
   }
 
   deleteTodo() {
     const todoId = '5560c4a0-344e-4c96-8619-9f1b670aa906'
-    this.todosService.deleteTodo(todoId).subscribe(() => {
-      this.todos = this.todos.filter(tl => tl.id !== todoId)
-    })
+    this.subscriptions.add(
+      this.todosService.deleteTodo(todoId).subscribe(() => {
+        this.todos = this.todos.filter(tl => tl.id !== todoId)
+      })
+    )
   }
 }
