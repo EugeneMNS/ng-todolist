549819b19afc7f386342c3e8a0deb5d52ae41b17
EugeneMNS <evgen.str4@gmail.com>
2022-10-24 16:39:57 +0300

    refactor: behaviorSubject & async pipe

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,58 +1,36 @@
 import { Component, OnDestroy, OnInit } from '@angular/core'
 import { HttpClient, HttpErrorResponse } from '@angular/common/http'
 import { BaseResponse, Todo, TodosService } from '../../services/todos.service'
-import { Subscription } from 'rxjs'
+import { Observable, Subscription } from 'rxjs'
 
 @Component({
   selector: 'inst-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss'],
 })
-export class TodosComponent implements OnInit, OnDestroy {
-  todos: Todo[] = []
+export class TodosComponent implements OnInit {
+  todos$!: Observable<Todo[]>
   error = ''
-  subscriptions: Subscription = new Subscription()
 
   constructor(private todosService: TodosService) {}
 
   ngOnInit(): void {
+    this.todos$ = this.todosService.todos$
     this.getTodos()
   }
 
-  ngOnDestroy(): void {
-    this.subscriptions.unsubscribe()
-  }
-
   getTodos() {
-    this.subscriptions.add(
-      this.todosService.getTodos().subscribe({
-        next: res => {
-          this.todos = res
-        },
-        error: (error: HttpErrorResponse) => {
-          this.error = error.message
-        },
-      })
-    )
+    this.todosService.getTodos()
   }
 
   createTodo() {
     const randomNumber = Math.floor(Math.random() * 100)
     const title = 'Angular ' + randomNumber
-    this.subscriptions.add(
-      this.todosService.createTodo(title).subscribe(res => {
-        const newTodo = res.data.item
-        this.todos.unshift(newTodo)
-      })
-    )
+    this.todosService.createTodo(title)
   }
 
   deleteTodo() {
-    const todoId = '5560c4a0-344e-4c96-8619-9f1b670aa906'
-    this.subscriptions.add(
-      this.todosService.deleteTodo(todoId).subscribe(() => {
-        this.todos = this.todos.filter(tl => tl.id !== todoId)
-      })
-    )
+    const todoId = '857e80be-c852-49c4-9183-ae6a78aab5a7'
+    this.todosService.deleteTodo(todoId)
   }
 }
