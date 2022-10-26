e8db138880501daaf6b671ecd0558363e32617d9
EugeneMNS <evgen.str4@gmail.com>
2022-10-26 10:33:35 +0300

    feat: add Mui lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { TodosModule } from './todos/todos.module'
 import { CoreModule } from './core/core.module'
 import { AppRoutingModule } from './app-routing.module'
 import { HttpClientModule } from '@angular/common/http'
-import { SharedModule } from './shared/shared.module'
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule, SharedModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
